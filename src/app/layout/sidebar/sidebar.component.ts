542749a935ceec39b9207bb7f9e6db71b8c0bbef
Ficode-Chhavinav <chhavinav@ficode.in>
2020-06-24 20:37:32 +0530

    Menu Toggle Function added

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -6,13 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
+  // Default Menu Status
   public showMoreMenu: boolean = false;
+  // Default Button Label
   public buttonLabel: any = 'More';
 
   constructor() {}
 
   ngOnInit(): void {}
 
+  /*  This function is used to show/hide More menu.
+   */
   toggleMenu() {
     this.showMoreMenu = !this.showMoreMenu;
     this.buttonLabel = this.showMoreMenu ? 'Less' : 'More';
