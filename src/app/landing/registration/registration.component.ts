84dff48ec92bfa1301d9b7f4c41e4bf436f5f349
Ficode-Chhavinav <chhavinav@ficode.in>
2020-07-08 18:13:13 +0530

    Registration Form Compressed and code optimize to as minimal as it could be.

--- src/app/landing/registration/registration.component.ts
+++ src/app/landing/registration/registration.component.ts
@@ -5,134 +5,47 @@ import { FieldsValidateService } from 'src/app/service/fields-validate.service';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.css'],
 })
 export class RegistrationComponent implements OnInit {
-  nameForm: FormGroup;
-  emailForm: FormGroup;
-  passwordForm: FormGroup;
-  mobileForm: FormGroup;
-  twoFactorForm: FormGroup;
-  termConditionForm: FormGroup;
-  banksDetailForm: FormGroup;
-  requestDataForm: FormGroup;
-  isNameForm = true;
-  isEmailForm = false;
-  isPasswordForm = false;
-  isMobileForm = false;
-  isTwoFactorForm = false;
-  isTermConditionForm = false;
-  isBanksDetailForm = false;
-  isRequestDataForm = false;
+  registrationForm: FormGroup;
+  currentActiveFormControl: string;
+  previousControlName: string;
   constructor(
     private assetsPipe: AssetsPathPipe,
     private formBuilder: FormBuilder,
-    private fieldsValidateService: FieldsValidateService,) { }
+    private fieldsValidateService: FieldsValidateService
+  ) {}
 
   ngOnInit() {
-    this.nameForm = this.formBuilder.group({
+    this.registrationForm = this.formBuilder.group({
       name: ['', Validators.required],
-    });
-
-    this.emailForm = this.formBuilder.group({
       email: ['', Validators.required],
-    });
-
-    this.passwordForm = this.formBuilder.group({
       password: ['', Validators.required],
-    });
-
-    this.mobileForm = this.formBuilder.group({
       mobile: ['', Validators.required],
-    });
-    this.twoFactorForm = this.formBuilder.group({
       twoFactorPassword: ['', Validators.required],
-    });
-    this.termConditionForm = this.formBuilder.group({
       age: ['', Validators.required],
       termAndCondition: ['', Validators.required],
     });
+    this.validateFormNextStep();
+  }
 
-    this.banksDetailForm = this.formBuilder.group({
-      // name: ['', Validators.required],
-      // email: ['', Validators.required],
+  /**
+   * Enable only next form
+   */
+  validateFormNextStep() {
+    const formValues = this.registrationForm.controls;
+    Object.entries(formValues).every((key, value) => {
+      const currentControlKey = key[0];
+      const currentControlStatus = key[1]['status'];
+      if (currentControlStatus === 'INVALID') {
+        this.fieldsValidateService.validateAllFormFields(this.registrationForm);
+        this.currentActiveFormControl = currentControlKey;
+        return false;
+      } else {
+        this.previousControlName = currentControlKey;
+        return true;
+      }
     });
-
-    this.requestDataForm = this.formBuilder.group({
-      // name: ['', Validators.required],
-      // email: ['', Validators.required],
-    });
-
-
-
-  }
-
-  /**
-   * Enable only next form 
-   * @param form group name 
-   */
-  onPrevious(form = null) {
-    this.disableAllForms();
-    this.showHideForms(form);
-  }
-
-  /**
-   *Enable only previous form 
-   * @param form group name 
-   */
-  onNext(form = null) {
-    this.disableAllForms();
-    this.showHideForms(form);
-  }
-  /**
-   *Enable perticular forms
-   * @param form group name 
-   */
-  showHideForms(fromName) {
-    console.log(fromName);
-    switch (fromName) {
-      case 'nameForm':
-        this.isNameForm = true;
-        break;
-      case 'emailForm':
-        this.isEmailForm = true;
-        break;
-      case 'passwordForm':
-        this.isPasswordForm = true;
-        break;
-      case 'mobileForm':
-        this.isMobileForm = true;
-        break;
-      case 'twoFactorForm':
-        this.isTwoFactorForm = true;
-        break;
-      case 'termConditionForm':
-        this.isTermConditionForm = true;
-        break;
-      case 'banksDetailForm':
-        this.isBanksDetailForm = true;
-        break;
-      case 'requestDataForm':
-        this.isRequestDataForm = true;
-        break;
-
-      default:
-        this.disableAllForms();
-        break;
-    }
-  }
-  /**
-  *Disable all forms
-  * @param form group name 
-  */
-  disableAllForms() {
-    this.isNameForm = false;
-    this.isPasswordForm = false;
-    this.isMobileForm = false;
-    this.isTwoFactorForm = false;
-    this.isTermConditionForm = false;
-    this.isBanksDetailForm = false;
-    this.isEmailForm = false;
-    this.isRequestDataForm = false;
   }
 }
