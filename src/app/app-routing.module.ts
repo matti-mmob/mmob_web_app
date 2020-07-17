72ae2424cd8caf6964b3c6ba0046057ea50a6774
Deepak Ramola <deepak@ficode.in>
2020-07-17 11:35:49 +0530

    new screen added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
     loadChildren: () => import('./dashboard/dashboard.module').then((routeModule) => routeModule.DashboardModule),
   },
   {
-    path: 'getting-setup',
+    path: '',
     loadChildren: () => import('./getting-setup/getting-setup.module').then((routeModule) => routeModule.GettingSetupModule),
   },
   {
